5966735c312870bbf8400dfe2d3fdb5b95ab9239
Iqbolshoh <148738947+Iqbolshoh@users.noreply.github.com>
2024-12-25 19:33:42 +0500

    commit

--- admin/index.php
+++ admin/index.php
@@ -107,8 +107,9 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">User List</h3>
+                                <h3 class="card-title">Seller List</h3>
                             </div>
+                            <!-- /.card-header -->
                             <div class="card-body">
                                 <table id="example2" class="table table-bordered table-hover">
                                     <thead>
@@ -152,42 +153,47 @@
                                     </tfoot>
                                 </table>
                             </div>
+                            <!-- /.card-body -->
                         </div>
+                        <!-- /.card -->
                     </div>
+                    <!-- /.col -->
                 </div>
             </section>
         </div>
 
+        <!-- Main Footer -->
         <?php include 'includes/footer.php'; ?>
     </div>
 
+    <!-- SCRIPTS -->
     <script src="../js/jquery.min.js"></script>
     <script src="../js/adminlte.js"></script>
-    <s cript src="../js/bootstrap.bundle.min.js"></script>
-        <scr ipt src="../js/jquery.dataTables.min.js">
-            </script>
-            <scr ipt src="../js/dataTables.bootstrap4.min.js">
-                </script>
+    <!-- Bootstrap 4 -->
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <!-- DataTables -->
+    <script src="../js/jquery.dataTables.min.js"></script>
+    <script src="../js/dataTables.bootstrap4.min.js"></script>
 
-                <scr ipt>
-                    $(function () {
-                    $('#example2').DataTable({
-                    "paging": true,
-                    "lengthChange": false,
-                    "searching": false,
-                    "ordering": true,
-                    "info": true,
-                    "autoWidth": false,
-                    "responsive": true,
-                    });
-                    });
-                    </script>
+    <script>
+        $(function () {
+            $('#example2').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true,
+            });
+        });
+    </script>
 
-                    <script>
-                        function changeStatus(userId, newStatus) {
-                            window.location.href = "change_status.php?userId=" + userId + "&newStatus=" + newStatus + "&userrole=seller";
-                        }
-                    </script>
+    <script>
+        function changeStatus(userId, newStatus) {
+            window.location.href = "change_status.php?userId=" + userId + "&newStatus=" + newStatus + "&userrole=user";
+        }
+    </script>
 
 </body>
 
