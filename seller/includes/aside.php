6d879d1f955a904f2fd2dae4255af664582facfb
Iqbolshoh <iilhomjonov777@gmail.com>
2025-01-02 23:31:18 +0500

    commit

--- seller/includes/aside.php
+++ seller/includes/aside.php
@@ -3,8 +3,8 @@ function active($Page, $Menu)
 { ?>
 
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <a href="/" class="brand-link">
-            <img src="../images/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <a href="./" class="brand-link">
+            <img src="../src/images/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                 style="opacity: .8">
             <span class="brand-text font-weight-light">AdminLTE 3</span>
         </a>
@@ -12,7 +12,7 @@ function active($Page, $Menu)
         <div class="sidebar">
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="../images/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                    <img src="../src/images/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block"><?php echo $_SESSION['name'] ?></a>
